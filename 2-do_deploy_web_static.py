dca353c1e918707c5ca55acdd8e6fb3560b7941b
Andres802 <reachandres@gmail.com>
2020-12-23 04:20:11 +0000

    do deply

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
- Fabric script that generates a .tgz archive from the contents of the web_static folder of your AirBnB Clone repo, using the function do_pack.
+ Fabric script that generates a .tgz archive
+ from the contents of the web_static folder of
+ your AirBnB Clone repo, using the function do_pack.
 """
 
 from fabric.api import local
@@ -9,14 +11,35 @@ from os.path import exists
 
 env.hosts = ['34.73.186.220', '35.237.108.29']
 
-def do_pack():
-    """function that pack archives"""
-    local('mkdir -p versions')
-    date = datetime.now()
-    tgz_file: local('tar -cvzf versions/web_static_{}{}{}{}{}{}.tgz web_static'
-                   .format(date.year, date.month, date.day,
-                           date.hour, date.minute, date.second), capture=True)
 
-    if tgz_file.succeeded:
-        return tgz_file
-    return None
+def do_pack():
+    """Function to compress files"""
+    local("mkdir -p versions")
+    file_1 = local("tar -czvf versions/web_static_{}.tgz web_static"
+                   .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")))
+    if file_1.failed:
+        return None
+    return file_1
+
+
+def do_deploy(archive_path):
+    """[summary]"""
+    if exists(archive_path):
+        file_path = archive_path.split("/")[1]
+        serv_path = "/data/web_static/releases/{}".format(
+            file_path.replace(".tgz", ""))
+        put('{}'.format(archive_path), '/tmp/')
+        run('mkdir -p {}'.format(serv_path))
+        run('tar -xzf /tmp/{} -C {}/'.format(
+            file_path,
+            serv_path))
+        run('rm /tmp/{}'.format(file_path))
+        run('mv -f {}/web_static/* {}/'.format(serv_path, serv_path))
+        run('rm -rf {}/web_static'.format(
+            serv_path))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {} /data/web_static/current'.format(
+            serv_path))
+        return True
+    else:
+        return False
