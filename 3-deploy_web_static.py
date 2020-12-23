f7ebe5b49acb65eaf6cce8a69a8e2dd5f8ada6a8
Andres802 <reachandres@gmail.com>
2020-12-23 06:23:21 +0000

    new script

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -5,7 +5,7 @@
  your AirBnB Clone repo, using the function do_pack.
 """
 
-from fabric.api import run, put, env
+from fabric.api import run, put, env, local
 from datetime import datetime
 from os.path import exists
 
