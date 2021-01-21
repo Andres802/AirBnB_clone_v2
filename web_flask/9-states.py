e4115d69c8c8c6a4eee2eaa92c3dea232f83ecf1
Andres802 <reachandres@gmail.com>
2021-01-21 18:52:53 +0000

    script fix

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -11,20 +11,32 @@ app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown(self):
-    """ Close session """
+def teardown_func(self):
+    """Will excecute after each request
+    Closes current sqlalchemy session
+    """
     storage.close()
 
 
-@app.route('/states')
-@app.route('/states/<id>')
-def state_and_state(id=None):
-    """ Display HTML with  STATES
-        or  CITIES with specific id """
-    state = ''
-    states = storage.all(State)
-    if id:
-        key = "State." + id
-        if key in states.keys():
-            state = states[key]
-    return render_template('9-states.html', id=id, state=state, states=states)
+@app.route('/states', strict_slashes=False)
+def states():
+    """Returns Hello HBNB
+    """
+    data = storage.all(State)
+    return render_template('9-states.html', states=data, mode="none")
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def states_id(id):
+    """Returns Hello HBNB
+    """
+    data = storage.all(State)
+    for state in data.values():
+        if state.id == id:
+            return render_template('9-states.html', state=state, mode="id")
+    else:
+        return render_template('9-states.html', states=data, mode="not")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
